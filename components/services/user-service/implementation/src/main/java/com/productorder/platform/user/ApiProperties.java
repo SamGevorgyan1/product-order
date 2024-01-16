40ed81baf15f7b3101d7dfbaa7e4fc6b94f7907c
samvel <samg05981@gmail.com>
2024-01-17 01:34:09 +0400

    initial punish

---
+++ components/services/user-service/implementation/src/main/java/com/productorder/platform/user/ApiProperties.java
@@ -0,0 +1,13 @@
+package com.productorder.platform.user;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProperties {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiProperties.class,args);
+    }
+}
